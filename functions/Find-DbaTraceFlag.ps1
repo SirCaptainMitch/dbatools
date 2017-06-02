12cce623e5334e6a5071354a34ec3ae8295ff328
Mitch Hamann <mhamann@shiplps.local>
2017-06-02 14:31:55 -0500

    breaking out base cmdlet

--- functions/Find-DbaTraceFlag.ps1
+++ functions/Find-DbaTraceFlag.ps1
@@ -7,33 +7,18 @@ Find a trace flag description, scope or Id by searching the trace flag index.
 .DESCRIPTION
 Searches through a JSON file of trace flags for an Id, Name, or parsing the description to see if you can find what you're looking for.
 
-.PARAMETER Tag
+.PARAMETER Description
 Finds all commands tagged with this auto-populated tag
 
-.PARAMETER Author
+.PARAMETER Scope
+Finds all commands tagged with this auto-populated tag
+
+.PARAMETER Id
 Finds all commands tagged with this author
-
-.PARAMETER MinimumVersion
-Finds all commands tagged with this auto-populated minimum version
-	
-.PARAMETER MaximumVersion
-Finds all commands tagged with this auto-populated maximum version
-	
-.PARAMETER Rebuild
-Rebuilds the index
-
-.PARAMETER Pattern
-Searches help for all commands in dbatools for the specified pattern and displays all results
-
-.PARAMETER Confirm
-Confirms overwrite of index
-
-.PARAMETER WhatIf
-Displays what would happen if the command is run
 
 .NOTES
 Tags: find
-Author: niphlod
+Author: Mitchell Hamann, @SirCaptainMitch, www.mitchellhamann.com
 
 dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
 Copyright (C) 2016 Chrissy LeMaire
@@ -45,173 +30,49 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 .LINK
-https://dbatools.io/Find-DbaCommand
+https://dbatools.io/Find-DbaTraceFlag
 
 .EXAMPLE
 Find-DbaCommand "snapshot"
 
 For lazy typers: finds all commands searching the entire help for "snapshot"
 
-.EXAMPLE
-Find-DbaCommand -Pattern "snapshot"
-
-For rigorous typers: finds all commands searching the entire help for "snapshot"
-
-.EXAMPLE
-Find-DbaCommand -Tag copy
-
-Finds all commands tagged with "copy"
-
-.EXAMPLE
-Find-DbaCommand -Tag copy,user
-
-Finds all commands tagged with BOTH "copy" and "user"
-
-.EXAMPLE
-Find-DbaCommand -Author chrissy
-
-Finds every command whose author contains our beloved "chrissy"
-
-.EXAMPLE
-Find-DbaCommand -Author chrissy -Tag copy
-
-Finds every command whose author contains our beloved "chrissy" and it tagged as "copy"
-
-.EXAMPLE
-Find-DbaCommand -Pattern snapshot -Rebuild
-
-Finds all commands searching the entire help for "snapshot", rebuilding the index (good for developers)
 #>
 	[CmdletBinding(SupportsShouldProcess = $true)]
 	Param (
-		[String]$Pattern,
-		[String[]]$Tag,
-		[String]$Author,
-		[String]$MinimumVersion,
-		[String]$MaximumVersion,
-		[switch]$Rebuild
+		[String]$Description,
+		[String[]]$Scope,
+		[String]$Id
 	)
 	BEGIN
-	{
-		$tagsRex = ([regex]'(?m)^[\s]{0,15}Tags:(.*)$')
-		$authorRex = ([regex]'(?m)^[\s]{0,15}Author:(.*)$')
-		$minverRex = ([regex]'(?m)^[\s]{0,15}MinimumVersion:(.*)$')
-		$maxverRex = ([regex]'(?m)^[\s]{0,15}MaximumVersion:(.*)$')
-		
-		function Get-DbaHelp([String]$commandname)
-		{
-			$thishelp = Get-Help $commandname -Full
-			$thebase = @{ }
-			$thebase.CommandName = $commandname
-			$thebase.Name = $thishelp.name
-			
-			## fetch the description
-			$thebase.Description = $thishelp.Description.text
-			
-			## fetch examples
-			$thebase.Examples = $thishelp.Examples | Out-String -Width 120
-			
-			## fetch help link
-			$thebase.Links = ($thishelp.relatedLinks).navigationLink.uri
-			
-			## fetch the synopsis
-			$thebase.Synopsis = $thishelp.Synopsis
-			
-			## store notes
-			$as = $thishelp.alertSet | Out-String -Width 120
-			
-			## fetch the tags
-			$tags = $tagsrex.Match($as).Groups[1].Value
-			if ($tags)
-			{
-				$thebase.Tags = $tags.Split(',').Trim()
-			}
-			## fetch the author
-			$author = $authorRex.Match($as).Groups[1].Value
-			if ($author)
-			{
-				$thebase.Author = $author.Trim()
-			}
-			
-			## fetch MinimumVersion
-			$MinimumVersion = $minverRex.Match($as).Groups[1].Value
-			if ($MinimumVersion)
-			{
-				$thebase.MinimumVersion = $MinimumVersion.Trim()
-			}
-			
-			## fetch MaximumVersion
-			$MaximumVersion = $maxverRex.Match($as).Groups[1].Value
-			if ($MaximumVersion)
-			{
-				$thebase.MaximumVersion = $MaximumVersion.Trim()
-			}
-			
-			[pscustomobject]$thebase
-		}
-		
-		function Get-DbaIndex()
-		{
-			if ($Pscmdlet.ShouldProcess($dest, "Recreating index"))
-			{
-				$dbamodule = Get-Module -Name dbatools
-				$allcommands = $dbamodule.ExportedCommands
-				$helpcoll = New-Object System.Collections.Generic.List[System.Object]
-				foreach ($c in $allcommands.GetEnumerator())
-				{
-					$x = Get-DbaHelp $c.Key
-					$helpcoll.Add($x)
-				}
-				# $dest = Get-DbaConfigValue -Name 'Path.TagCache' -Fallback "$(Resolve-Path $PSScriptRoot\..)\dbatools-index.json"
-				$dest = "$moduledirectory\bin\dbatools-index.json"
-				$helpcoll | ConvertTo-Json | Out-File $dest -Encoding UTF8
-			}
-		}
-		
+	{				
 		$moduledirectory = (Get-Module -Name dbatools).ModuleBase
+        $idxfile = "$moduledirectory\bin\TraceFlag-index.json"
 	}
 	PROCESS
 	{
-		# $idxfile = Get-DbaConfigValue -Name 'Path.TagCache' -Fallback "$(Resolve-Path $PSScriptRoot\..)\dbatools-index.json"
-		$idxfile = "$moduledirectory\bin\dbatools-index.json"
-		if (!(Test-Path $idxfile) -or $Rebuild)
-		{
-			Write-Verbose "Rebuilding index into $idxfile"
-			$swrebuild = [system.diagnostics.stopwatch]::startNew()
-			Get-DbaIndex
-			Write-Verbose "Rebuild done in $($swrebuild.Elapsedmilliseconds)ms"
 			
-		}
 		$consolidated = Get-Content -Raw $idxfile | ConvertFrom-Json
 		$result = $consolidated
+
 		if ($Pattern.length -gt 0)
 		{
-			$result = $result | Where-Object { $_.psobject.properties.value -like "*$Pattern*" }
+			$result = $result | Where-Object Description -Contains $Description
 		}
 		
-		if ($Tag.length -gt 0)
+		if ($Name.length -gt 0)
 		{
 			foreach ($t in $Tag)
 			{
-				$result = $result | Where-Object Tags -contains $t
+				$result = $result | Where-Object Name -contains $t
 			}
 		}
 		
-		if ($Author.length -gt 0)
+		if ($Id.length -gt 0)
 		{
-			$result = $result | Where-Object Author -like "*$Author*"
+			$result = $result | Where-Object TraceFlagId -eq $Id
 		}
 		
-		if ($MinimumVersion.length -gt 0)
-		{
-			$result = $result | Where-Object MinimumVersion -ge $MinimumVersion
-		}
-		
-		if ($MaximumVersion.length -gt 0)
-		{
-			$result = $result | Where-Object MaximumVersion -le $MaximumVersion
-		}
-		
-		Select-DefaultView -InputObject $result -Property CommandName, Synopsis
+		Select-DefaultView -InputObject $result -Property TraceFlagId, Scope, Description
 	}
 }
